d84ce483c56daa2a8e9224dc4256b2a761fed735
James Scott <scottj1426@gmail.com>
2019-08-19 12:34:24 -0500

    calculator component

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -1,14 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Input } from "semantic-ui-react";
 
 class calculator extends Component {
-    render() {
-        return (
-            <div>
-                Hi Welcome to The Paylocity Coding Challenge
-                
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      employeeName: '',
+      numberOfDependants: 0,
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleDependantChange = this.handleDependantChange.bind(this);
+  }
+  handleNameChange(event) {
+    this.setState({
+      employeeName: event.target.value
+    });
+    console.log(this.state);
+  }
+  handleDependantChange(event) {
+    this.setState({
+      numberOfDependants: event.target.value
+    });
+    console.log(this.state);
+  }
+
+  calculator(){
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hi Welcome to The Paylocity Coding Challenge</h1>
+        <Input
+          value={this.state.employeeName}
+          placeholder="Employee Name"
+          type="text"
+          onChange={this.handleNameChange}
+        />
+        <Input
+          value={this.state.numberOfDependants}
+          placeholder="# of Employee Dependants"
+          type="text"
+          onChange={this.handleDependantChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default calculator;
