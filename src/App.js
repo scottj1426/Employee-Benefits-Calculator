5c64f2c55ff7fa1803ca783a13c2a0b6e07e9c70
James Scott <scottj1426@gmail.com>
2019-08-19 12:05:48 -0500

    app setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './calculator'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Calculator/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/calculator.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class calculator extends Component {
+    render() {
+        return (
+            <div>
+                Hi Welcome to The Paylocity Coding Challenge
+                
+            </div>
+        );
+    }
+}
+
+export default calculator;
