b7a157d1ffc71c38737c2f824c28e232dc7018c2
James Scott <scottj1426@gmail.com>
2019-08-19 13:07:59 -0500

    calculations complete

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
-import { Input } from "semantic-ui-react";
+import { Input, Form, Button } from "semantic-ui-react";
+
+const dependantOptions = [
+  { key: "1", text: "1", value: "1" },
+  { key: "2", text: "2", value: "2" },
+  { key: "3", text: "3", value: "3" }
+];
 
 class calculator extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      employeeName: '',
+      employeeName: "",
       numberOfDependants: 0,
+      calculatedCost: ""
     };
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleDependantChange = this.handleDependantChange.bind(this);
+    this.calculate = this.calculate.bind(this);
   }
   handleNameChange(event) {
     this.setState({
@@ -24,25 +32,43 @@ class calculator extends Component {
     console.log(this.state);
   }
 
-  calculator(){
+  calculate() {
+    //6000 is years worth of dependants 500 * 12 months
+    //12000 is years worth for employee
+    let dependantCost = this.state.numberOfDependants * 6000;
+    let cost = 12000 + dependantCost;
+    this.setState({
+      calculatedCost: cost
+    });
+    console.log(cost);
   }
 
   render() {
     return (
       <div>
-        <h1>Hi Welcome to The Paylocity Coding Challenge</h1>
-        <Input
-          value={this.state.employeeName}
-          placeholder="Employee Name"
-          type="text"
-          onChange={this.handleNameChange}
-        />
-        <Input
-          value={this.state.numberOfDependants}
-          placeholder="# of Employee Dependants"
-          type="text"
-          onChange={this.handleDependantChange}
-        />
+        <h1>Hi Welcome to The Coding Challenge</h1>
+        <Form>
+          <Input
+            value={this.state.employeeName}
+            placeholder="Employee Name"
+            type="text"
+            onChange={this.handleNameChange}
+          />
+          <Input
+            value={this.state.numberOfDependants}
+            placeholder="# of dependants"
+            type="text"
+            onChange={this.handleDependantChange}
+          />
+        </Form>
+        <div>
+          <Button onClick={this.calculate}>Calculate Cost</Button>
+        </div>
+        <div>
+            <h1> 
+                Calculated Benefites for {this.state.employeeName} for 2019 is {this.state.calculatedCost}
+            </h1>
+        </div>
       </div>
     );
   }
